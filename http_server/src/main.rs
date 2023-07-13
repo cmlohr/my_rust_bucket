f00281d63051f1610cc12c648b2991b99aec21c6
cmlohr <cmlohr@outlook.com>
2023-07-13 10:23:39 -0500

    basic http server final

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,10 +1,6 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
 use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
-
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,7 +14,15 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024]; // 1024 bytes buffer
     stream.read(&mut buffer).unwrap();
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let request_line = request.lines().next().unwrap();
+    let parts: Vec<&str> = request_line.split(" ").collect();
+    let method = parts[0];
+    let path = parts[1];
+    let version = parts[2];
+    println!("{} {} {}", method, path, version);
     let responce = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
     stream.write(responce.as_bytes()).unwrap();
     stream.flush().unwrap();
